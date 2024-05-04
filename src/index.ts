c282fd99ce63f89c68bb44b9d30029f428ed94c1
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-05-04 17:21:12 +0530

    Feat: Setup the basic backend with typescript

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, {Express} from 'express';
+import serverConfig from './config/serverConfig';
+
+
+const app: Express = express();
+
+
+app.get("/", (_req, res)=>{
+    res.send("server is live");
+})
+
+
+app.listen(serverConfig.PORT, ()=>{
+    console.log("Server started at 5502")
+})
