686e0fbcdce70dcdd7ce6b1166a6ee0c06ec3de6
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-05-06 11:40:25 +0530

    Bull mq based redis queue infra up and runnign

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express, { Express } from "express";
 
 import serverConfig from "./config/serverConfig";
+import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
+import SampleWorker from "./workers/SampleWorker";
 
 const app: Express = express();
 
 app.use("/api", apiRouter);
 
-app.listen(serverConfig.PORT, () => {
+app.listen(serverConfig.PORT, async () => {
   console.log("Server started at 5502");
+  SampleWorker("SampleQueue");
+  await sampleQueueProducer("SampleJob", {
+    name: "gyanendra kumar",
+    company: "wise neosco",
+  });
 });
